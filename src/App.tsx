be24399f7616b320be5b8e0916a2d977944455ac
faheem-maf <faahmad@mafcarrefour.com>
2020-09-21 08:53:22 +0530

    feat: deployment via github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import './App.css';
 import Dashboard from 'pages/dashboard';
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import { HashRouter, Redirect, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <main className="app">
-      <BrowserRouter>
+      <HashRouter basename='/'>
         <Switch>
           <Route path="/dashboard" component={Dashboard} />
           <Redirect from="/" to="dashboard" exact />
           <Route path="/*" render={() => <div>Broken route</div>} />
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     </main>
   );
 }
