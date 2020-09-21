d1960142cdb2e226739fd2badf8900275051602c
faheem-maf <faahmad@mafcarrefour.com>
2020-09-21 09:01:52 +0530

    Updates

---
+++ precache-manifest.4eb5beaa17c7a42af29c4cfb04eef9c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa90772a2fb891272206e2e3cb7ec6eb",
+    "url": "/stock-dashboard/index.html"
+  },
+  {
+    "revision": "9b92b71229d9625191d6",
+    "url": "/stock-dashboard/static/css/main.a72495fd.chunk.css"
+  },
+  {
+    "revision": "c23b5a9b8f60bab179d7",
+    "url": "/stock-dashboard/static/js/2.c68b08d3.chunk.js"
+  },
+  {
+    "revision": "17061da1f707efaaf35913ea934465ff",
+    "url": "/stock-dashboard/static/js/2.c68b08d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b92b71229d9625191d6",
+    "url": "/stock-dashboard/static/js/main.eb985a6a.chunk.js"
+  },
+  {
+    "revision": "163c5e33304bbce80f4f",
+    "url": "/stock-dashboard/static/js/runtime-main.4b981781.js"
+  }
+]);
